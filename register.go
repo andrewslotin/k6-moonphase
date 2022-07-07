3a2c6da48dc57a92ba04d706d803ca35ac6def83
Andrew Slotin <andrew.slotin@gmail.com>
2022-07-07 16:40:16 +0200

    Read Stormglass API key from env

--- register.go
+++ register.go
@@ -1,9 +1,13 @@
 package moonphase
 
-import "go.k6.io/k6/js/modules"
+import (
+	"os"
+
+	"go.k6.io/k6/js/modules"
+)
 
 const stormglassURL = "https://api.stormglass.io/v2"
 
 func init() {
-	modules.Register("k6/x/moonphase", New(stormglassURL))
+	modules.Register("k6/x/moonphase", New(stormglassURL, os.Getenv("STORMGLASS_API_KEY")))
 }
